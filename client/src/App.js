0dbe7f6f768f0c3a4f007576477d3b856067f469
soumyadepsinha <s.sinha2701@gmail.com>
2020-09-23 19:56:44 +0530

    add popup for new entry

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 const App = () => {
   const [logEntries, setLogEntries] = useState([]);
   const [showPopup, setShowPopup] = useState({});
+  const [addEntry, setAddEntry] = useState(null);
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
@@ -23,17 +24,21 @@ const App = () => {
     })();
   }, []);
 
-  const showAddNewEntry = (e) => {
-    console.log(e);
-  }
+  const showAddNewEntry = (event) => {
+    const [longitude, latitude] = event.lngLat;
+    setAddEntry({
+      latitude,
+      longitude,
+    });
+  };
 
   return (
     <ReactMapGL
       {...viewport}
       mapStyle="mapbox://styles/thecjreynolds/ck117fnjy0ff61cnsclwimyay"
-      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      mapboxApiAccessToken={process.env.MAPBOX_TOKEN}
       onViewportChange={(nextViewport) => setViewport(nextViewport)}
-      onDbclick={showAddNewEntry}
+      onDblClick={showAddNewEntry}
     >
       {logEntries.map((entry) => (
         <React.Fragment key={entry._id}>
@@ -43,9 +48,11 @@ const App = () => {
             longitude={entry.longitude}
           >
             <div
-              onClick={() => setShowPopup({
-                [entry._id]: true,
-              })}
+              onClick={() =>
+                setShowPopup({
+                  [entry._id]: true,
+                })
+              }
             >
               <svg
                 className="marker-pin"
@@ -55,7 +62,7 @@ const App = () => {
                 }}
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                stroke-width="1"
+                stroke-width="1.5"
                 fill="none"
                 stroke-linecap="round"
                 stroke-linejoin="round"
@@ -65,27 +72,68 @@ const App = () => {
               </svg>
             </div>
           </Marker>
-          {
-            showPopup[entry._id] ? (
-              <Popup
-                latitude={entry.latitude}
-                longitude={entry.longitude}
-                closeButton={true}
-                closeOnClick={false}
-                dynamicPosition={true}
-                onClose={() => setShowPopup({})}
-                anchor="top" >
-                <div className="popup">
-                  <h3>{entry.title}</h3>
-                  <p>{entry.comments}</p>
-                  <small>Visited on: {new Date(entry.visitDate).toLocaleDateString()}</small>
-                  {entry.image && <img src={entry.image} alt={entry.title} />}
-                </div>
-              </Popup>
-            ) : null
-          }
+          {showPopup[entry._id] ? (
+            <Popup
+              latitude={entry.latitude}
+              longitude={entry.longitude}
+              closeButton={true}
+              closeOnClick={false}
+              dynamicPosition={true}
+              onClose={() => setShowPopup({})}
+              anchor="top"
+            >
+              <div className="popup">
+                <h3>{entry.title}</h3>
+                <p>{entry.comments}</p>
+                <small>
+                  Visited on: {new Date(entry.visitDate).toLocaleDateString()}
+                </small>
+                {entry.image && <img src={entry.image} alt={entry.title} />}
+              </div>
+            </Popup>
+          ) : null}
         </React.Fragment>
       ))}
+      {addEntry ? (
+        <React.Fragment>
+          <Marker
+            latitude={addEntry.latitude}
+            longitude={addEntry.longitude}
+          >
+            <div>
+              <svg
+                className="marker-pin"
+                style={{
+                  width: `${8 * viewport.zoom}px`,
+                  height: `${8 * viewport.zoom}px`,
+                }}
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="1.5"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                <circle cx="12" cy="10" r="3"></circle>
+              </svg>
+            </div>
+          </Marker>
+          <Popup
+            latitude={addEntry.latitude}
+            longitude={addEntry.longitude}
+            closeButton={true}
+            closeOnClick={false}
+            dynamicPosition={true}
+            onClose={() => setAddEntry(null)}
+            anchor="top" >
+            <div className="popup">
+              <h3>Add Your Entry</h3>
+            </div>
+          </Popup>
+        </React.Fragment>
+      ) : null
+      }
     </ReactMapGL>
   );
 };
